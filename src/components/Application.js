5b83501f9dc16bf28bef61a3f5a6e7cd41514047
Sebastian <lynchseb@gmail.com>
2021-02-18 17:39:50 -0500

    add youtube API components, connect youtube components to Resium entities

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,20 +4,29 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from "react-dom";
 import Sidebar from "react-sidebar";
 
-//Component Imports
+// Model Test
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button';
+
+//Sidebar Component Imports
 import MaterialTitlePanel from "./MaterialTitlePanel";
 import SidebarContent from "./SidebarContent";
-import Playlist from "./Playlist";
 
-// import MaterialTitlePanel from "./material_title_panel";
-// import SidebarContent from "./sidebar_content";
+//Youtube API components
+import SearchBar from './Searchbar';
+import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 // Enabling HRM
 import { hot } from "react-hot-loader/root";
 
 // Resium imports
-import { Viewer, Entity, PointGraphics, EntityDescription } from "resium";
-import { Cartesian3, createWorldTerrain } from "cesium";
+import { Viewer, Entity, PointGraphics, EntityDescription, CustomDataSource } from "resium";
+import { Cartesian3, createWorldTerrain, Color } from "cesium";
+
+// Style imports
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Default Sidebar Styles
 const styles = {
@@ -35,69 +44,80 @@ const styles = {
 const terrainProvider = createWorldTerrain();
 
 const getUserPreferences = [
-  {
-    name: "Tokyo",
-    position: Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100),
-    content: "test"
-  },
+
   {
     name: "Toronto",
+    position: Cartesian3.fromDegrees(-74.0707383, 40.7117244, 100),
+  
+    key: 1
+     
+  },
+  {
+    name: "Tokyo",
     position: Cartesian3.fromDegrees(43.6532, 79.3832, 100),
-    content: "test"
-  },
-  {
-    name: "Bolivia",
-    position: Cartesian3.fromDegrees(-100.0707383, 133.7117244, 100),
-    content: "test"
-  },
-  {
-    name: "Peru",
-    position:  Cartesian3.fromDegrees(145.0707383, 165.7117244, 100),
-    content: "test"
-  },
-  {
-    name: "Bangkok",
-    position: Cartesian3.fromDegrees(23.0707383, 78.7117244, 100),
-    content: "test"
+    
+    key: 2
   }
 ]
+
+
+const termFromSearchBar = "?part=snippet&maxResults=5&key=AIzaSyCuPWQiKkYwa8pbCPmdmCJJVm53jMAsQ0A&q=hockey"
   
-  // Cartesian3.fromDegrees(15.0707383, 30.7117244, 100),
-  // Cartesian3.fromDegrees(42.0707383, 55.7117244, 100),
-  // Cartesian3.fromDegrees(77.0707383, -145.7117244, 100)
+const test = function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+   
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
+export default hot(function Application(props) {
 
-class Application extends React.Component {
-  constructor(props) {
-    super(props);
+  const [state, setState] = useState({
+    docked: false,
+    open: false,
+    transitions: true,
+    videos: [],
+    selectedVideos: null
 
-    this.state = {
-      docked: true,
-      open: false,
-      transitions: true,
-      shadow: true,
-    };
-
-    this.renderPropCheckbox = this.renderPropCheckbox.bind(this);
-    this.onSetOpen = this.onSetOpen.bind(this);
-    this.menuButtonClick = this.menuButtonClick.bind(this);
-  }
+  })
 
   //Sidebar methods begin
-  onSetOpen(open) {
-    this.setState({ open });
+
+  const onSetOpen = (open) => {
+    setState(prev => ({...prev, open}))
   }
 
-  menuButtonClick(ev) {
+  const menuButtonClick = (ev) => {
     ev.preventDefault();
-    this.onSetOpen(!this.state.open);
+    onSetOpen(!state.open);
   }
 
-  renderPropCheckbox(prop) {
+  const renderPropCheckbox = (prop) => {
     const toggleMethod = ev => {
-      const newState = {};
-      newState[prop] = ev.target.checked;
-      this.setState(newState);
+      const newState = {...state};
+      newState.docked = ev.target.checked
+      setState(prev => ({...prev, ...newState}));
     };
 
     return (
@@ -106,7 +126,7 @@ class Application extends React.Component {
           <input
             type="checkbox"
             onChange={toggleMethod}
-            checked={this.state[prop]}
+            checked={state.docked}
             id={prop}
           />
           {prop}
@@ -114,63 +134,102 @@ class Application extends React.Component {
       </p>
     );
   }
+
   // Sidebar methods end
 
  
-  userPreferences = getUserPreferences.map((entity) => {
+
+  // Youtube API methods begin
+  const handleSubmit = async (termFromSearchBar) => {
+    const response = await youtube.get('/search', {
+        params: {
+            q: termFromSearchBar
+        }
+    })
+    setState(prev => ({...prev,  videos: response.data.items}))
+       
+    
+};
+    const handleVideoSelect = (video) => {
+    setState(prev => ({...prev, selectedVideo: video}))
+}
+
+  // YouTube API methods end
+
+  // Entity Builder Begin
+ 
+  const userPreferences = getUserPreferences.map((entity) => {
     return ( 
-    <Entity position={entity.position} name={entity.name}>
-      <PointGraphics pixelSize={15} />
-      <EntityDescription>
-        <h1>{entity.content}</h1>
-      </EntityDescription>
-    </Entity> 
+      <Entity key={entity.key} position={entity.position} name={entity.name} description={"Hello World"} point={{ pixelSize: 15, color: Color.BLUE }}>
+        <EntityDescription resizeInfoBox={true}>
+          <div className='ui container' style={{marginTop: '1em'}}>
+            <SearchBar handleFormSubmit={handleSubmit}/>
+            <div className='ui grid'>
+              <div className="ui row">
+                <div className="five wide column">
+                  <VideoList handleVideoSelect={handleVideoSelect} videos={state.videos}/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </EntityDescription>
+      </Entity> 
     )
   })
 
-  render() {
-    const sidebar = <SidebarContent />;
+  //Entity Builder end
 
-    const contentHeader = (
-      <span>
-        {!this.state.docked && (
-          <a onClick={this.menuButtonClick} href="#" style={styles.contentHeaderMenuLink}>
-            =
-          </a>
-        )}
-        <span>oneWorld {["docked"].map(this.renderPropCheckbox)}</span>
-        
-      </span>
-    );
+  const sidebar = <SidebarContent />;
 
-    const sidebarProps = {
-      sidebar,
-      docked: this.state.docked,
-      sidebarClassName: "custom-sidebar-class",
-      // contentId: "custom-sidebar-content-id",
-      open: this.state.open,
-      onSetOpen: this.onSetOpen
-    };
+  const contentHeader = (
+    <span className='span-head'>
+      
+        <a onClick={menuButtonClick} href="#" style={styles.contentHeaderMenuLink}>
+          =
+        </a> oneWorld
+      {renderPropCheckbox()}
+      
+    </span>
+  );
+
+  const sidebarProps = {
+    sidebar,
+    docked: state.docked,
+    sidebarClassName: "custom-sidebar-class",
+    // contentId: "custom-sidebar-content-id",
+    open: state.open,
+    onSetOpen
+  };
 
     return (
-      <nav>
-          <Sidebar {...sidebarProps}>
-            <MaterialTitlePanel title={contentHeader}>
-              <div style={styles.content}>
-                <Viewer terrainProvider={terrainProvider}>
-                  {this.userPreferences}
-                </Viewer>
-              </div>
-            </MaterialTitlePanel>
-          </Sidebar>
-      </nav>
+      <main>
+       
+        <nav>
+            <Sidebar {...sidebarProps}>
+              <MaterialTitlePanel title={contentHeader}>
+                <div style={styles.content}>
+                  <Viewer infobox={true} fullscreenButton={true} terrainProvider={terrainProvider} onClick={(e, entity) => console.log(entity)}>
+                    <CustomDataSource>
+                    { userPreferences }
+
+                    </CustomDataSource>
+                  </Viewer>
+                </div>
+              </MaterialTitlePanel>
+            </Sidebar>
+        </nav>
+      <footer>
+      <div className="eleven wide column">
+                    <VideoDetail video={state.selectedVideo}/>
+                </div> 
       
-    
+      </footer>
+      </main>
     );
   }
-}
+)
 
-{/* <main>
+/* <main>
 <nav>
     <Sidebar {...sidebarProps}>
       
@@ -183,11 +242,11 @@ class Application extends React.Component {
                 </Viewer>
         </div>
       </MaterialTitlePanel>
-</main> */}
+</main> */
 
-export default hot(Application)
+// export default hot(Application)
 
-ReactDOM.render(<Application />, document.getElementById("root"));
+// ReactDOM.render(<Application />, document.getElementById("root"));
 
 
 
--- src/components/Playlist.js
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
-const KEY = "AIzaSyCuPWQiKkYwa8pbCPmdmCJJVm53jMAsQ0A"
-
-export default function Playlist(props) {
-
-}
