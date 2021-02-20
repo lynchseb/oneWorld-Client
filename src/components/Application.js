b3bfc87b8503796d603b34d16057ba2be3fb948c
Sebastian <lynchseb@gmail.com>
2021-02-20 16:00:52 -0500

    change description of Tokyo

--- src/components/Application.js
+++ src/components/Application.js
@@ -55,7 +55,7 @@ const getUserPreferences = [
   {
     name: "Tokyo",
     position: Cartesian3.fromDegrees(43.6532, 79.3832, 100),
-    description: "Asian",
+    description: "Asia",
     key: 2
   }
 ]
